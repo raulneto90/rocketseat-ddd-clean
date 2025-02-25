5afe1a22bfcaf3bb4b830e482dc46ef8f2ebc61a
Raul Neto <raulneto90@gmail.com>
2025-02-24 21:40:04 -0300

    feat: add Slug value object and integrate it into Question entity; include tests for Slug creation

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from 'node:crypto';
+import type { Slug } from './value-objects/slug';
 
 interface ConstructorParams {
   title: string;
   content: string;
+  slug: Slug;
   authorId: string;
   id?: string;
 }
@@ -11,12 +13,14 @@ export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public slug: Slug;
   public authorId: string;
 
   constructor(props: ConstructorParams) {
     this.title = props.title;
     this.content = props.content;
     this.authorId = props.authorId;
+    this.slug = props.slug;
     this.id = props.id ?? randomUUID();
   }
 }
---
+++ src/domain/entities/value-objects/slug.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest';
+import { Slug } from './slug';
+
+describe('Slug Value Object', () => {
+  it('should be able to create a new slug from a text', () => {
+    const slug = Slug.createFromText('Example question title');
+
+    expect(slug.value).toBe('example-question-title');
+  });
+});
